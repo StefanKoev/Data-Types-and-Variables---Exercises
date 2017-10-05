8492d87bef061b33357e3be7fdb1a40e2f29d95c
Stefan Koev <koev.stefan@gmail.com>
2017-10-05 17:17:14 +0300

    Create 12 Rectangle Properties

---
+++ 12 Rectangle Properties/Rectangle-Properties.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double with = double.Parse(Console.ReadLine());
+            double leight = double.Parse(Console.ReadLine());
+
+            double perimeter = 2 * (with + leight);
+            double area = with * leight;
+            double diagonal = Math.Sqrt(with * with + leight * leight);
+
+            Console.WriteLine(perimeter);
+            Console.WriteLine(area);
+            Console.WriteLine(diagonal);
+        }
+    }
+}
