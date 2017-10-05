83c4b19699e6f675410f4b83a4234cd64c1f2185
Stefan Koev <koev.stefan@gmail.com>
2017-10-05 17:06:26 +0300

    Create 06 Strings and Objects

---
+++ 06 Strings and Objects/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_and_Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var string1 = Console.ReadLine();
+            var string2 = Console.ReadLine();
+
+            string result = String.Concat(string1, " ", string2);
+
+            Console.WriteLine(result);
+        }
+    }
+}
