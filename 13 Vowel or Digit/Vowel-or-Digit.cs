8a3e9ec4cf4ff7c68875e8b203b4a8407a51422c
Stefan Koev <koev.stefan@gmail.com>
2017-10-05 17:18:31 +0300

    Create 13 Vowel or Digit

---
+++ 13 Vowel or Digit/Vowel-or-Digit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vowel_or_Digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char simbol = char.Parse(Console.ReadLine());
+
+            if (char.IsDigit(simbol))
+            {
+                Console.WriteLine("digit");
+            }
+            else if ("aeiou".Contains(simbol))
+            {
+                Console.WriteLine("vowel");
+            }
+            else
+            {
+                Console.WriteLine("other");
+            }
+        }
+    }
+}
