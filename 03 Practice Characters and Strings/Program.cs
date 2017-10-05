d87f7d2ff29e2a34b470f4ffadb42de40749853a
Stefan Koev <koev.stefan@gmail.com>
2017-10-05 17:01:07 +0300

    Create 03 Practice Characters and Strings

---
+++ 03 Practice Characters and Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Characters_and_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line1 = "Software University";
+            char line2 = 'B';
+            char line3 = 'y';
+            char line4 = 'e';
+            string line5 = "I love programming";
+            Console.WriteLine(line1);
+            Console.WriteLine(line2);
+            Console.WriteLine(line3);
+            Console.WriteLine(line4);
+            Console.WriteLine(line5);
+        }
+    }
+}
