243dd1196ff919153869f3e90faf9ceb86be3f63
Stefan Koev <koev.stefan@gmail.com>
2017-10-05 17:11:48 +0300

    Create 09 Reverse Characters

---
+++ 09 Reverse Characters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            char new1 = char.Parse(Console.ReadLine());
+            char new2 = char.Parse(Console.ReadLine());
+            char new3 = char.Parse(Console.ReadLine());
+
+            string text = string.Concat(new1, new2, new3);
+            string reverseText = new string(text.Reverse().ToArray());
+
+            Console.WriteLine(reverseText);
+        }
+    }
+}
