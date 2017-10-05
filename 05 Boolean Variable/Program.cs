557b8a81faf359569937d94fcc032e4aa917f33a
Stefan Koev <koev.stefan@gmail.com>
2017-10-05 17:05:02 +0300

    Create 05 Boolean Variable

---
+++ 05 Boolean Variable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = Console.ReadLine();
+
+            bool result = Convert.ToBoolean(value);
+
+            if (result)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
