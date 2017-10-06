31695dad93eeb29a5b1e0a5110ed899ebdd74189
Stefan Koev <koev.stefan@gmail.com>
2017-10-06 21:45:35 +0300

    Create 16 Comparing Floats

---
+++ 16 Comparing Floads/Comparin-Floads.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparing_Floats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float firstNumber = float.Parse(Console.ReadLine());
+            float secondtNumber = float.Parse(Console.ReadLine());
+
+            double difference = 0.000001;
+
+            if (Math.Abs(firstNumber - secondtNumber) < difference)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
