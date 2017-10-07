327248aa02546f3e0f43a5687ae78e2bd6aaf5a5
Stefan Koev <koev.stefan@gmail.com>
2017-10-07 16:31:31 +0300

    Create 18 Differet Integer Size

---
+++ 18 Diferet Integer Size/Different-Integers-Size.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Different_Integers_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+
+            decimal number = decimal.Parse(n);
+
+            if (number > long.MaxValue || number < long.MinValue)
+            {
+                Console.WriteLine($"{number} can't fit in any type");
+            }
+            else
+            {
+                Console.WriteLine($"{number} can fit in:");
+                if (number >= sbyte.MinValue && number <= sbyte.MaxValue)
+                {
+                    Console.WriteLine("* sbyte");
+                }
+                if (number >= byte.MinValue && number <= byte.MaxValue)
+                {
+                    Console.WriteLine("* byte");
+                }
+                if (number >= short.MinValue && number <= short.MaxValue)
+                {
+                    Console.WriteLine("* short");
+                }
+                if (number >= ushort.MinValue && number <= ushort.MaxValue)
+                {
+                    Console.WriteLine("* ushort");
+                }
+                if (number >= int.MinValue && number <= int.MaxValue)
+                {
+                    Console.WriteLine("* int");
+                }
+                if (number >= uint.MinValue && number <= uint.MaxValue)
+                {
+                    Console.WriteLine("* uint");
+                }
+                if (number >= long.MinValue && number <= long.MaxValue)
+                {
+                    Console.WriteLine("* long");
+                }
+            }
+        }
+    }
+}
